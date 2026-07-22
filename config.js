46c494e3beb1cd78e90c5d320668a7c8c91c63b2
JhulioCezar <131815120+JhulioCezar@users.noreply.github.com>
2026-07-22 11:08:14 -0500

    Add configuration for Supabase connection

---
+++ config.js
@@ -0,0 +1,4 @@
+const CONFIG = {
+    SUPABASE_URL: 'https://oikjeybrhyiccaqrtcws.supabase.co',
+    SUPABASE_ANON_KEY: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im9pa2pleWJyaHlpY2NhcXJ0Y3dzIiwicm9sZSI6ImFub24iLCJpYXQiOjE3Nzk5NzUxMDYsImV4cCI6MjA5NTU1MTEwNn0.OJOOyBCfqJl5HtYJfPFs-1LESN3e3vMdZthwNFGJ7xo'
+};
